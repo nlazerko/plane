dc5dbef272052703f3b341d3a881d214a1bcb743
nlazerko <lazerkokolya@gmail.com>
2022-02-19 11:02:50 +0300

    getTour.js

--- scripts/service/getTour.js
+++ scripts/service/getTour.js
@@ -3,7 +3,7 @@ const URL_API = 'https://61f4662310f0f7001768c90f.mockapi.io/api/airplane';
 const getData = () => fetch(URL_API)
     .then((response) => {
         if(response.ok) {
-         return response.json();
+            return response.json();
         }
         throw new Error(response.status);
     })
